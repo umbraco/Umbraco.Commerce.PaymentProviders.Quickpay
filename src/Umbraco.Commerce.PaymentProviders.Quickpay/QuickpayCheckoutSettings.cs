fcb3af52fe2ea250804cdc6fef319e4dd2a01755
Dinh Tran <trd@umbraco.dk>
2024-08-02 10:59:11 +0700

    Upgrade to commerce v14 (#10)
    * Upgrade to commerce v14
    
    * fine tuning v14
    
    * remove packages.lock.json

--- src/Umbraco.Commerce.PaymentProviders.Quickpay/QuickpayCheckoutSettings.cs
+++ src/Umbraco.Commerce.PaymentProviders.Quickpay/QuickpayCheckoutSettings.cs
@@ -4,18 +4,15 @@ namespace Umbraco.Commerce.PaymentProviders.Quickpay
 {
     public class QuickpayCheckoutSettings : QuickpaySettingsBase
     {
-        [PaymentProviderSetting(Name = "Auto Fee",
-            Description = "Flag indicating whether to automatically calculate and apply the fee from the acquirer.",
+        [PaymentProviderSetting(
             SortOrder = 1100)]
         public bool AutoFee { get; set; }
 
-        [PaymentProviderSetting(Name = "Auto Capture",
-            Description = "Flag indicating whether to immediately capture the payment, or whether to just authorize the payment for later (manual) capture.",
+        [PaymentProviderSetting(
             SortOrder = 1200)]
         public bool AutoCapture { get; set; }
 
-        [PaymentProviderSetting(Name = "Framed",
-            Description = "Flag indicating whether to allow opening payment page in iframe.",
+        [PaymentProviderSetting(
             SortOrder = 1300)]
         public bool Framed { get; set; }
     }
